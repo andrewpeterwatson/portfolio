8b5f5b8f952374d486f5cf56327f12e5233d76ab
Andrew Watson <andrewpeterwatson@gmail.com>
2016-03-13 08:59:11 -0700

    style changes

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,15 @@ var myProjs = [];
 
 function Project(articleObj) {
   this.name = articleObj.name;
-  this.pubdate = articleObj.pubdate;
+  // this.pubdate = articleObj.pubdate;
   this.body = articleObj.body;
+  this.image = articleObj.image;
 }
 Project.prototype.toHtml = function() {
   var $newProject = $('article.template').clone();
   $newProject.find('.article-title').text(this.name);
-  $newProject.find('time[pubdate]').attr('datetime', this.pubdate);
   $newProject.find('.article-body').html(this.body);
+  $newProject.find('.article-src').attr('src', this.image);
   $newProject.removeClass('template');
   return $newProject;
 };
@@ -21,3 +22,4 @@ projects.forEach(function(obj) {
 myProjs.forEach(function(a) {
   $('#articles').append(a.toHtml());
 });
+$('.template').hide();
--- scripts/pages.js
+++ scripts/pages.js
@@ -1,13 +1,14 @@
 
 var projects = [
-  {
+    {
     name: 'POMPEII',
-    pubdate: '03/01/2016',
     body: '<p>Chuck ipsum. Multiple people have died from Chuck Norris giving them the finger. Chuck Norris doesn’t need to swallow when eating food. Crop circles are Chuck Norris way of telling the world that sometimes corn needs to lie the f*ck down. Chuck Norris ordered a Big Mac at Burger King, and got one.</p>',
+    image: 'media/refImages/POMPEII.jpg'
   },
   {
-    name: 'In the Mind of a Zombie',
-    pubdate: '02/17/2016',
+    name: 'Salmon Shack',
     body: '<p>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris.</p>',
+    image: 'media/refImages/fish.jpg'
+    // url: "",
   },
 ];
