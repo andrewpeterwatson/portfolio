3223b0b77b61c65881f7d89208382e7814609c30
Andrew Watson <andrewpeterwatson@gmail.com>
2016-03-14 19:40:33 -0700

    added toggle function to porject body

--- scripts/app.js
+++ scripts/app.js
@@ -10,12 +10,6 @@ Project.prototype.toHtml = function() {
   var template = Handlebars.compile(templateSrc);
   return template(this);
 };
-$(".imgSrc").on("click", function(e){
-  e.preventDefault();
-  console.log("clicked");
-  $(".article-body").removeAttr("hidden");
-})
-
 projects.forEach(function(obj) {
   myProjs.push(new Project(obj));
 });
